d70491318690985ca2dffd73919870ea0cb41cab
Sohom Das <78586384+das-sohom@users.noreply.github.com>
2022-09-01 23:00:28 +0100

    Add files via upload

---
+++ Boruta.R
@@ -0,0 +1,138 @@
+library(randomForest)
+library(Boruta)
+########################################
+#     Feature Selection -Boruta
+########################################
+
+# dataframe
+
+# one_three - with pre-filter nrow(82)
+# One_Three2 - without pre-filter nrow(143)
+# two_four- with pre-filter nrow(82)
+# Two_Four2 - without pre-filter nrow(143)
+# one_four- with pre-filter nrow(82)
+# One_Four2 - without pre-filter nrow(143)
+# lung.cor_rem2- with pre-filter nrow(82)
+# lung.cor2 - without pre-filter nrow(143)
+
+
+R=10
+K=5  # using 5 folds
+n=nrow(one_four)
+folds = cut(1:n, K, labels=FALSE)
+st=cm=list()
+acc.rf=auc.rf=acc.rf_final=rf.p_final=auc.rf_final=numeric(K)
+accuracy.rf=precision.rf=recall.rf=f1.score.rf=numeric(K)
+acc.mean=mean_acc=mean_auc=pre.mean=rec.mean=auc.mean=f1.mean=numeric(R)
+acc_for_box=auc_for_box=acc_for_box2=auc_for_box2=numeric(1)
+forward.matrix1 = matrix(nrow = 50, ncol = ncol(one_four))
+colnames(forward.matrix1) = colnames(one_four)
+forward.matrix1 = forward.matrix1[,-82]
+x=1
+y=1
+a=1
+for(r in 1:R){
+  forward.matrix = matrix(nrow = K, ncol = ncol(one_four))
+  colnames(forward.matrix) = colnames(one_four)
+  forward.matrix = forward.matrix[,-82]
+  
+  for (k in 1:K){
+    i.train=which(folds!=k)
+    i.test=which(folds==k)
+    x.train=one_four[i.train,]
+    x.test=one_four[i.test,]
+    
+    rf.out=randomForest(stg~.,data=x.train)
+    rf.pred = predict(rf.out,x.test,'class')
+    tb.rf=table(rf.pred,x.test$stg)
+    acc.rf[k]=sum(diag(tb.rf)) / sum(tb.rf)
+    acc_for_box[x]=acc.rf[k]
+    
+    
+    
+    rf.p=predict(rf.out,x.test,'prob')[,2]
+    auc.rf[k] = roc(x.test$stg, rf.p)$auc
+    auc_for_box[x]=auc.rf[k]
+    x=x+1
+    
+    
+    boruta.train <- Boruta(stg~., data = x.train,maxRuns = 100, doTrace = 1)
+    st[[k]]= which(boruta.train$finalDecision=='Confirmed')
+    len[a]= length(which(boruta.train$finalDecision=='Confirmed'))
+    
+    if(length(which(boruta.train$finalDecision=='Confirmed'))==0){
+      forward.matrix[k,] = 0
+      forward.matrix1[a,] = 0
+    } else{
+      forward.matrix[k,which(boruta.train$finalDecision=='Confirmed')] = 1
+      forward.matrix[k,-which(boruta.train$finalDecision=='Confirmed')] = 0
+      forward.matrix1[a,which(boruta.train$finalDecision=='Confirmed')] = 1
+      forward.matrix1[a,-which(boruta.train$finalDecision=='Confirmed')] = 0
+      a=a+1
+    }
+  }
+  
+  mean_acc[r] = mean(acc.rf)
+  mean_auc[r] = mean(auc.rf)
+  
+  fs=c()
+  for(i in 1:ncol(forward.matrix)){
+    if((sum(forward.matrix[,i])/nrow(forward.matrix)) >= 0.20){
+      fs = c(fs, colnames(forward.matrix)[i])
+    }
+  }
+  fs = c(fs, "stg")
+  fs_sel[[r]]= fs
+  for (k in 1:K){
+    i.train2=which(folds!=k)
+    i.test2=which(folds==k)
+    x.train2=one_four[i.train2,fs]
+    x.test2=one_four[i.test2,fs]
+    
+    rf.sel=randomForest(stg~.,data=x.train2)
+    
+    rf.pred_final = predict(rf.sel,x.test2,'class')
+    tb.rf_final=table(rf.pred_final,x.test2$stg)
+    acc.rf_final[k]=sum(diag(tb.rf_final)) / sum(tb.rf_final)
+    acc_for_box2[y]=acc.rf_final[k]
+    
+    rf.p_final=predict(rf.sel,x.test2,'prob')[,1]
+    auc.rf_final[k] =roc(x.test2$stg, rf.p_final)$auc
+    auc_for_box2[y]=auc.rf_final[k]
+    
+    roc=roc(x.test2$stg, rf.p_final)
+    proc[[k]]=roc
+    cm=caret::confusionMatrix(rf.pred_final,x.test2$stg, positive="stage4")
+    accuracy.rf[k] = round(cm$overall[1],2)
+    precision.rf[k] = round(cm$byClass[5],2)
+    recall.rf[k] = round(cm$byClass[6],2)
+    f1.score.rf[k] = round(cm$byClass[7],2)
+    roc.outer[[k]] = roc
+    auc.outer[k] = round(roc$auc,2)
+    y=y+1
+  }
+  
+  acc.mean[r]=mymean(accuracy.rf)
+  auc.mean[r]=mymean(auc.rf_final)
+  pre.mean[r]=mymean(precision.rf)
+  rec.mean[r]=mymean(recall.rf)
+  f1.mean[r]=mymean(f1.score.rf)
+  
+}
+
+box=cbind(acc_for_box,acc_for_box2,auc_for_box,auc_for_box2)
+colnames(box)=c('Acc No FS','Acc with FS','AUC No FS','AUC with FS')
+boxplot(box)
+
+round(mean(acc_for_box),2)
+round(sd(acc_for_box),2)
+round(mean(auc_for_box),2)
+
+round(mean(acc_for_box2),2)
+round(sd(acc_for_box2),2)
+round(mean(auc_for_box2),2)
+
+round(mean(pre.mean),2)
+round(mean(rec.mean),2)
+round(mean(f1.mean),2)
+round(mean(len),0)
